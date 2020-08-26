3ae3452d0245bc20f50ccfd2ce10444c895227c4
iuryabreu <iurydeabreu@gmail.com>
2020-08-25 22:01:35 -0300

    Updates

---
+++ precache-manifest.cb75917dbb8590a5d0853278fb2d747a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36ffb2a8f795387b134b65129af8be2",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "d9f79e9c0e9d97e13c2f",
+    "url": "/todo-list/static/css/main.97bb3819.chunk.css"
+  },
+  {
+    "revision": "c25faf4524f47cf7ac78",
+    "url": "/todo-list/static/js/2.797d6e14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.797d6e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f79e9c0e9d97e13c2f",
+    "url": "/todo-list/static/js/main.9b6fcc36.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
